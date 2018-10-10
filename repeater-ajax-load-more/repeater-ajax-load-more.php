b3755642de3c929a1c0bb5eead5ebe89fa3f93c0
Mikhail Zavalko <mikhailzavalko@gmail.com>
2018-10-10 15:56:28 +1000

    Go back
    Go back

--- repeater-ajax-load-more/repeater-ajax-load-more.php
+++ repeater-ajax-load-more/repeater-ajax-load-more.php
@@ -1,18 +1,36 @@
-// add action for logged in users
-add_action('wp_ajax_my_repeater_show_more', 'my_repeater_show_more');
+<?php 
+	
+	/*
+			this is an example of how to create a repeater that will show a specific number of rows
+			and add a link (button) to display more rows
+			
+			this code would generally be added to your functions.php file
+			
+			Note: This will only work for a single repeater field. You will need to add different functions
+			with different names to use this on multiple repeater field
+			
+			The code in this file should go into your function.php file
+			or be included by your functions.php file
+			
+			Please note that this example uses jQuery to perform the AJAX request
+			You must enqueue jQuery using a wp_enqueue_scripts action
+	*/
+	
+	// add action for logged in users
+	add_action('wp_ajax_my_repeater_show_more', 'my_repeater_show_more');
 	// add action for non logged in users
-add_action('wp_ajax_nopriv_my_repeater_show_more', 'my_repeater_show_more');
-
-function my_repeater_show_more() {
+	add_action('wp_ajax_nopriv_my_repeater_show_more', 'my_repeater_show_more');
+	
+	function my_repeater_show_more() {
 		// validate the nonce
-	if (!isset($_POST['nonce']) || !wp_verify_nonce($_POST['nonce'], 'my_repeater_field_nonce')) {
-		exit;
-	}
+		if (!isset($_POST['nonce']) || !wp_verify_nonce($_POST['nonce'], 'my_repeater_field_nonce')) {
+			exit;
+		}
 		// make sure we have the other values
-	if (!isset($_POST['post_id']) || !isset($_POST['offset'])) {
-		return;
-	}
-		$show = 2; // how many more to show
+		if (!isset($_POST['post_id']) || !isset($_POST['offset'])) {
+			return;
+		}
+		$show = 4; // how many more to show
 		$start = $_POST['offset'];
 		$end = $start+$show;
 		$post_id = $_POST['post_id'];
@@ -21,10 +39,10 @@ function my_repeater_show_more() {
 		// and add the content to this string, but I find
 		// object buffers make the code easier to work with
 		ob_start();
-		if (have_rows('reviews_product', $post_id)) {
-			$total = count(get_field('reviews_product', $post_id));
+		if (have_rows('load_more_example_repeater', $post_id)) {
+			$total = count(get_field('load_more_example_repeater', $post_id));
 			$count = 0;
-			while (have_rows('reviews_product', $post_id)) {
+			while (have_rows('load_more_example_repeater', $post_id)) {
 				the_row();
 				if ($count < $start) {
 					// we have not gotten to where
@@ -33,32 +51,7 @@ function my_repeater_show_more() {
 					$count++;
 					continue;
 				}
-				?>
-
-				<div class="comment">
-					<div class="comment_header">
-						<div class="info">
-							<div class="line">
-								<span class="label">Name:</span>
-								<span class="text"><?php the_sub_field('review_name'); ?></span>
-							</div>
-							<div class="line">
-								<span class="label">Date review:</span>
-								<span class="text"><?php the_sub_field('review_date'); ?></span>
-							</div>
-						</div>
-						<span data-rate="<?php the_sub_field('review_rating'); ?>" class="rating rating-rev rate-<?php the_sub_field('review_rating'); ?>">
-							<span class="star"></span>
-							<span class="star"></span>
-							<span class="star"></span>
-							<span class="star"></span>
-							<span class="star"></span>
-						</span>
-					</div>
-					<div class="comment_content"><?php the_sub_field('review_text'); ?></div>
-				</div>
-
-				<?php 
+				?><li><?php the_sub_field('sub_field'); ?></li><?php 
 				$count++;
 				if ($count == $end) {
 					// we've shown the number, break out of loop
@@ -87,3 +80,5 @@ function my_repeater_show_more() {
 		$group = json_decode($json, true);
 		acf_add_local_field_group($group);
 	} // end function load_repeater_more_example_group
+	
+?>
